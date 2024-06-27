199ec6bc4f8fa80ef7295837f01224dde120c97b
Saksham Verma <skshm.090898@gmail.com>
2024-06-28 02:11:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 require('express-async-errors');
-const path = require('path');
 
 const express = require('express');
 const app = express();
@@ -11,7 +10,7 @@ const mainRouter = require('./src/routes/main');
 const notFoundMiddleware = require('./src/middleware/not-found');
 const errorMiddleware = require('./src/middleware/error-handler');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./public'));
 app.use(express.json());
 
 
